1a65fcdf936e5a3ee7ed74f7ef483f22f98946f5
Shengyuan Yan <39942337+yanshengyuan@users.noreply.github.com>
2022-06-04 22:13:33 +0800

    Add files via upload

---
+++ TransRLUNet/run.sh
@@ -0,0 +1,1 @@
+python3 ./train.py --arch Uformer --gpu '0,1,2,3,4,5,6,7' --train_ps 128 --train_dir ../GoPro/train --val_dir ../GoPro/test --embed_dim 16 --warmup --batch_size 64 --nepoch 500 --lr_initial 0.0003
